ee3aa62e5fdd9501574b170e4dddf45c6d20fd3b
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-03-05 10:01:37 -0500

    Actualización de comentarios

--- src/Data/Database.php
+++ src/Data/Database.php
@@ -26,8 +26,9 @@ class Database
     private string $defaultTable;
 
     /**
-     * Inicializa la clase para intereactuar con la base de datos.
-     * @param array $data_connection
+     * Método constructor.
+     * @param array $data_connection Array asociativo con los datos para una conexión mysqli 
+     * [ hostname:string, username:string, password:string, database:string, port:int, socket:string ]
      * @param string $table Nombre de la tabla por default.
      */
     public function __construct(private array $dataConnection,string $table = null)
@@ -38,6 +39,7 @@ class Database
     /**
      * Retorna el objecto msqli que representa una conexion de una base de datos de MySQL.
      * en caso de que el objeto no este inicializa de creara.
+     * @throws ApiException Retorna un excepción en caso que no poder establecer una conexión con la base de datos.
      */
     public function getConnection():mysqli
     {
@@ -77,8 +79,10 @@ class Database
 
     /**
      * Ejecuta una consulta preparada en la base de datos.
-     * @param string $slq comando sql a ejecutar.
-     * @param array $params Paramentros de la consulta sql.
+     * @param string $sql Comando SQL a ejecutar en la base de datos, los parametros se deben definir con "?".
+     * @param array $params Array de los parametros de la consulta SQL
+     * @throws ApiException En caso de no poder establecer la conexión con la base de datos
+     * @throws ApiException En caso de haber un error estableces los bind_params
      */
     public function query(string $sql, ?array $params = null):bool|mysqli_result
     {
@@ -87,6 +91,10 @@ class Database
 
     /**
      * Ejecuta un comando en la base de datos.
+     * @param string $sql Comando SQL a ejecutar en la base de datos, los parametros se deben definir con "?".
+     * @param array $params Array de los parametros de la consulta SQL
+     * @throws ApiException En caso de no poder establecer la conexión con la base de datos
+     * @throws ApiException En caso de haber un error estableces los bind_params
      */
     private function queryMySql(string $sql, array $params = null):bool|mysqli_result
     {
@@ -141,6 +149,7 @@ class Database
 
     /**
      * Retorna un array de los errores obtenidos durante las consultas sql.
+     * @return object[]
      */
     public function getErrors():array
     {
@@ -148,7 +157,7 @@ class Database
     }
 
     /**
-     * Confima y carga los cambios realizados en la en la base de datos.
+     * Confirma y carga los cambios realizados en la en la base de datos.
      */
     public function commit():bool{
         $this->lastCommand = null;
@@ -156,7 +165,11 @@ class Database
     }
 
     /**
-     * Ejecuta una cosulta sql en la base de datos
+     * Ejecuta una consulta preparada en la base de datos, es similar query con la diferencia que retorna un objeto DatabaseResutl.
+     * @param string $sql Comando SQL a ejecutar en la base de datos, los parametros se deben definir con "?".
+     * @param array $params Array de los parametros de la consulta SQL
+     * @throws ApiException En caso de no poder establecer la conexión con la base de datos
+     * @throws ApiException En caso de haber un error estableces los bind_params
      */
     public function execute(string $slq, array $params = null):DatabaseResult
     {
@@ -164,8 +177,8 @@ class Database
     }
 
     /**
-     * Arma y ejecuta un select en la base de datos con los parametros ingresados.
-     * @param string $table Nombre de la tabla por default es la tabla establecida por defecto con setDefaultTable
+     * Ejecuta un select en la base de datos con los parametros ingresados.
+     * @param string $table Nombre de la tabla.
      * @param array $fields Campos o array de los campos a seleccionar de la tabla por defecto es "*".
      * @param string|array $condition Condición where de la filas a seleccionas, puese ser un string o un array 
      * donde el primer item es un string con la condicion preparadao y el segundo un array con los parametros.
@@ -197,8 +210,10 @@ class Database
 
     /**
      * Inserta datos en la tabla designada.
-     * @param array|object $params Un array asositativo o un objeto con los parametros.
+     * @param array|object $params Un array asositativo o un objeto con los parametros, el nombre de las key del array o del objeto deben
+     * concordar con los nombre de los campos esperada por la base de datos..
      * @param string $table Nombre de la tabla, por defecto es el nombre de la tabla establecido.
+     * @throws ApiException En caso de haber un problema con la conexión, el bind_params, o preparación de la consulta.
      */
     public function insert(array|object $params, string $table = null):DatabaseResult
     {
@@ -220,6 +235,7 @@ class Database
      * @param array|object $params array asociativo o un objecto con los parametros a editar.
      * @param string|array $condition Condición where de la filas a seleccionas, puese ser un string o un array 
      * donde el primer item es un string con la condicion preparadao y el segundo un array con los parametros.
+     * @throws ApiException En caso de haber un problema con la conexión, el bind_params, o preparación de la consulta
      */
     public function update(array|object $params, string|array $condition, string $table = null):DatabaseResult
     {
@@ -241,6 +257,7 @@ class Database
      * Elimina un datos de una tabla.
      * @param string|array $condition Condición where de la filas a seleccionas, puese ser un string o un array 
      * donde el primer item es un string con la condicion preparadao y el segundo un array con los parametros.
+     * @throws ApiException En caso de haber un problema con la conexión, el bind_params, o preparación de la consulta
      */
     public function delete(array|string $condition, string $table = null):DatabaseResult
     {
@@ -255,7 +272,8 @@ class Database
     /**
      * Ejecuta una función en la base de datos.
      * @param string $name Nombre de la funcion a ejecutar.
-     * @param array $params array de los parametros
+     * @param array|null $params array de los parametros
+     * @throws ApiException En caso de haber un problema con la conexión, el bind_params, o preparación de la consulta
      */
     public function function(string $name, array $params = null):DatabaseResult
     {
@@ -267,7 +285,8 @@ class Database
     /**
      * Ejecuta un procedimiento en la base de datos.
      * @param string $name Nombre del procedimiento a ejecutar.
-     * @param array $params array de los parametros
+     * @param array|null $params array de los parametros.
+     * @throws ApiException En caso de haber un problema con la conexión, el bind_params, o preparación de la consulta
      */
     public function procedure(string $name, array $params = null):DatabaseResult
     {
