70a3a357663882432dcb1c27081eedb7615794a9
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-04-02 13:37:22 -0500

    Se implemento la función 'getBody'

--- src/Controller.php
+++ src/Controller.php
@@ -15,4 +15,14 @@ class Controller
     {
         
     }
+
+     /**
+     * Obtiene el contenido del body decodificado
+     * El contenido del body debe estar en formato json.
+     * @param bool $assoc Si es true retornara un array asositivo en caso controlario un objeto
+     */
+    public function getBody(bool $assoc = false):object|array|int|float|string
+    {
+        return json_decode(file_get_contents("php://input"), $assoc);
+    }
 }
