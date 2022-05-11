656d6bf3018a21200318c822ee7c5ad131efc58e
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-05-11 15:02:34 -0500

    Se mofica el funcionamiento de las Exceptiones

--- src/ApiException.php
+++ src/ApiException.php
@@ -32,12 +32,13 @@ class ApiException extends Exception
      * @param (string|string[])[] $messages_developer
      * @param Throwable $th exception del cath
      */
-    public function __construct(array $messages_developer, ?Throwable $th = null, $text_body = 'Error - server', private int $responeCode = 500)
+    public function __construct(array $messages_developer, ?Throwable $th = null, string $text_body = null, private int $responeCode = 500)
     {
         try {
             
             $this->messageDeveloper = $messages_developer;
-            $this->textBody = $text_body;
+            $this->textBody = $text_body ? $text_body : '[ERROR SERVER]';
+            Api::response()->httpResponseCode(1);
     
             parent::__construct('', 0, $th);
             if ($th){
@@ -47,10 +48,14 @@ class ApiException extends Exception
                 $this->file = $th->getFile();
             }
         } catch (\Throwable $th) {
-            throw new Exception("Errro a inilizalizar la clase");
+            throw $th;
+            throw new Exception("Errro a inilizalizar la clase: " . ApiException::class . "\n Message: " . $th->getMessage());
         }
     }
 
+    /**
+     * @return int Rotorna el códido de estado de la petición HTTP
+     */
     public function getHttpResponseCode():int
     {
         return $this->responeCode;
@@ -79,75 +84,24 @@ class ApiException extends Exception
      */
     public function getTextBody():string
     {
-        return $this->textBody;
-    }
-
-    public function getError():ErrorRegister
-    {
-        return ErrorRegister::__load($this);
-    }
-    /**
-     * Imprime el error en el body
-     */
-    public function echo():void
-    {
-        // Carmaso la informacion del errro a un array asosiativo
-        // $content = [
-        //     "api"=>Api::getConfig()->getName(),
-        //     "date"=> date('Y-m-d H:i:s',time()) . "z",
-        //     "httpRequest"=>[
-        //         'url' => $_GET['url'],
-        //         'method'=> Router::getMethod(),
-        //         'ip'=>ClientInfo::getIp(),
-        //         'device'=>ClientInfo::getDevice(),
-        //         'platform'=>ClientInfo::getPlatform()
-        //     ],
-        //     "description"=>"",
-        //     "messageDeveloper"=>$this->messageDeveloper,
-        //     "mensajeError"=>$this->message,
-        //     "code"=>$this->getCode(),
-        //     "file"=>$this->getFile(),
-        //     "line"=>$this->getLine(),
-        //     "trace"=>$this->getTrace()
-        // ];
-
-        // // Códigicamos al formato JSON
-        // $content = json_encode($content);
-
-        // $file_path = Api::getDir() . "/nv-panel/errors/list.txt";
-        // $dir_name = dirname($file_path);
+        $text = "<<< [ RUNTIME ERROR ] >>>\n";
+        $text .= "\nMessage developer:\n";
+        foreach ($this->messageDeveloper as $value){
+            if (is_string($value)){
+                $text .= "  $value\n";
+            }else{
+                foreach ($value as $subs){
+                    $subs = str_replace("\n", "\n    ", $subs);
+                    $text .= "    $subs\n";
+                }
+            }
+        }
         
-        // if (!file_exists(dirname($dir_name))) mkdir(dirname($dir_name));
-        // if (!file_exists($dir_name)) mkdir($dir_name);
-
-        // $file = fopen($file_path, 'a+');
-        // fputs($file, "$content,\n");
-        // fclose($file);
-
-        // header('content-type: text, charset=utf-8');
-        // if (Api::getAppConfig()->debug()){
-        //     echo "Mensaje del desarrollador:\n";
-        //     foreach($this->getMessageDeveloper() as $item){
-        //         if (is_string($item)){
-        //             echo "$item\n";
-        //         }else{
-        //             foreach($item as $sub_item){
-        //                 echo "\t" . (is_string($sub_item) ? $sub_item : json_encode($sub_item, 128)) . "\n";
-        //             }
-        //         }
-        //     }
-        //     echo "\n\nMessaje error: " . $this->getMessage() . "\n";
-        //     echo "-----------------------------------------------------\n";
-        //     echo "Código:    " . $this->getCode() . "\n";
-        //     echo "Archivo:    " . $this->getFile() . "\n";
-        //     echo "Linea:    " . $this->getLine() . "\n";
-        //     echo "\n-----------------------------------------------------\n";
-        //     echo "Rastro:";
-        //     echo json_encode($this->getTrace(), 128);
-        // }else{
-        //     echo $this->textBody;
-        // }
-        // http_response_code($this->responeCode);
-        exit;
+        $text .= "\nMessage error:\n";
+        $text .= "  " . $this->message . "\n\n";
+        $text .= "Code: " . $this->code . "\n";
+        $text .= "File: " . $this->file . "\n";
+        $text .= "Line: " . $this->line . "\n";
+        return $text;
     }
 }
